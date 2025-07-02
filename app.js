2e33937dbc3e2e19fa7bb70c222ea56721ee6fd7
Arpit <omagrahari55@gmail.com>
2025-07-02 20:29:41 +0530

    cors setuped

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 import express from 'express';
 import { config } from 'dotenv';
 import medrouter from './router/med_router.js';
+import cors from "cors"
 config({
-    path:"./.env"
-})
+    path: "./.env"
+});
+
 const app = express();
+const corsOptions = {
+    origin: 'http://localhost:3000', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    credentials: true,
+};
+
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(medrouter);
+
 export default app;
